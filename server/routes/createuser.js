6ffdd165c676c178ebe02245629a9e863c6076eb
Simon Vegsund <sve034@uit.no>
2024-12-11 22:12:36 +0100

    added bcrypt library and a hashfunction for password

--- server/routes/createuser.js
+++ server/routes/createuser.js
@@ -1,25 +1,40 @@
 import express from 'express';
 import User from '../models/User.js';
 const router = express.Router();
+import bcrypt from 'bcrypt';
+
+async function encryptPassword(plaintextPassword, saltRounds) {
+	try {
+		const salt = await bcrypt.genSalt(saltRounds);
+		const hash = await bcrypt.hash(plaintextPassword, salt);
+		return hash;
+	} catch (err) {
+		throw new Error('Error hashing password');
+	}
+}
 
 // Route to create a user
 router.post('/', async (req, res) => {
 	try {
 		const { firstName, lastName, email, password } = req.body;
 
-		if (!firstName || !lastName || !email || !password) {
-			return res.status(400).json({ msg: 'All fields are required' });
-		}
+		// if (!firstName || !lastName || !email || !password) {
+		// 	return res.status(400).json({ msg: 'All fields are required' });
+		// }
+
 		const existingUser = await User.findOne({ email });
 		if (existingUser) {
 			return res.status(400).json({ msg: 'Email already in use.' });
 		}
 
+		const saltRounds = 10;
+		const hash = await encryptPassword(password, saltRounds);
+
 		const newUser = new User({
 			firstName,
 			lastName,
 			email,
-			password,
+			password: hash,
 		});
 		await newUser.save();
 
